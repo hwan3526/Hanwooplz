2184889931af47794060dd963429b6454fd5cc9e
hwan3526 <hwan3526@gmail.com>
2024-08-08 04:16:28 +0900

    fix: resolve the problem that cannot login immediately after registration

--- hanwooplz/account/views.py
+++ hanwooplz/account/views.py
@@ -54,8 +54,8 @@ def register(request):
         form = CustomUserCreationForm(request.POST)
         if form.is_valid():
             form.save()
-            username = form.get('username')
-            password = form.get('password1')
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password1')
             user = authenticate(username=username, password=password)
             if user:
                 login(request, user)
