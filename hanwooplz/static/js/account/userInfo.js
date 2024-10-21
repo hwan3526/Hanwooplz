de21114cee29cd3e0a49d8a5815024c52c784046
hwan3526 <hwan3526@gmail.com>
2024-10-22 00:42:09 +0900

    refactor: simplify the scripts including tab active function on account:user_info page

--- hanwooplz/static/js/account/userInfo.js
+++ hanwooplz/static/js/account/userInfo.js
@@ -1,74 +1,27 @@
-const portfolioTab = document.getElementById('portfolio-tab');
-const projectTab = document.getElementById('project-tab');
-const questionTab = document.getElementById('question-tab');
-const answerTab = document.getElementById('answer-tab');
-
-const portfolioPost = document.getElementById('portfolio-post');
-const projectPost = document.getElementById('project-post');
-const questionPost = document.getElementById('question-post');
-const answerPost = document.getElementById('answer-post');
-
-portfolioTab.addEventListener('click', function () {
-    portfolioTab.style.backgroundColor = '#dddddd';
-    projectTab.style.backgroundColor = '';
-    questionTab.style.backgroundColor = '';
-    answerTab.style.backgroundColor = '';
-
-    portfolioPost.style.display = 'grid';
-    projectPost.style.display = 'none';
-    questionPost.style.display = 'none';
-    answerPost.style.display = 'none';
-});
-
-projectTab.addEventListener('click', function () {
-    portfolioTab.style.backgroundColor = '';
-    projectTab.style.backgroundColor = '#dddddd';
-    questionTab.style.backgroundColor = '';
-    answerTab.style.backgroundColor = '';
-
-    portfolioPost.style.display = 'none';
-    projectPost.style.display = 'grid';
-    questionPost.style.display = 'none';
-    answerPost.style.display = 'none';
-});
-
-questionTab.addEventListener('click', function () {
-    portfolioTab.style.backgroundColor = '';
-    projectTab.style.backgroundColor = '';
-    questionTab.style.backgroundColor = '#dddddd';
-    answerTab.style.backgroundColor = '';
-
-    portfolioPost.style.display = 'none';
-    projectPost.style.display = 'none';
-    questionPost.style.display = 'grid';
-    answerPost.style.display = 'none';
-});
-
-answerTab.addEventListener('click', function () {
-    portfolioTab.style.backgroundColor = '';
-    projectTab.style.backgroundColor = '';
-    questionTab.style.backgroundColor = '';
-    answerTab.style.backgroundColor = '#dddddd';
-
-    portfolioPost.style.display = 'none';
-    projectPost.style.display = 'none';
-    questionPost.style.display = 'none';
-    answerPost.style.display = 'grid';
-});
-
 const profilePostTab = document.querySelectorAll('.profile-post-tab');
-const profilePostBody = document.querySelectorAll('.profile-post-body');
-let maxHeight = 0;
+const profilePostGrid = document.querySelectorAll('.profile-post-grid');
 
 for (let i = 0; i < 4; i++) {
-    profilePostTab[i].click();
-    if (maxHeight < profilePostBody[i].offsetHeight) {
-        maxHeight = profilePostBody[i].offsetHeight;
-    }
+    profilePostTab[i].addEventListener('click', function () {
+        for (let j = 0; j < 4; j++) {
+            if (j == i) {
+                profilePostTab[j].classList.add('active');
+                profilePostGrid[j].classList.add('active');
+            } else {
+                profilePostTab[j].classList.remove('active');
+                profilePostGrid[j].classList.remove('active');
+            }
+        }
+    });
 }
 
-profilePostBody.forEach(function (body) {
-    body.style.height = maxHeight + 'px';
-});
+const profilePost = document.querySelector('.profile-post');
 
-portfolioTab.click();
+let heights = []
+for (let i = 0; i < 4; i++) {
+    profilePostTab[i].click();
+    heights.push(profilePostGrid[i].offsetHeight);
+}
+
+profilePost.style.height = 81 + Math.max(...heights) + 'px';
+profilePostTab[0].click();
